6acdfb906a8894948b8150524bef601ade993ce4
ni9gas <lordqieztcloud@gmail.com>
2025-03-22 17:30:10 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Google Fonts Preconnect */}
-        <link rel="icon" href="favicon.svg" type="image/x-icon" />
+        <link rel="icon" href="favicon.svg" type="image/svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
